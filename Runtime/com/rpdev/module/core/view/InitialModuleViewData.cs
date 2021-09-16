abacc936e3a543e4a3cdd09d1a9c14deb8013216
Roman <rpugamedev@gmail.com>
2021-09-16 15:31:20 +0600

    no message

--- Runtime/com/rpdev/module/core/view/InitialModuleViewData.cs
+++ Runtime/com/rpdev/module/core/view/InitialModuleViewData.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace com.rpdev.foundation.module.core.view {
     
+    [Serializable]
     public class InitialModuleViewData {
         public Transform  parent_container;
         public Vector3    position;
