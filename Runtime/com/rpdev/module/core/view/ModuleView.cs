c1875b08e6c194d003de757a1e84f7c18f9bab31
Roman <rpugamedev@gmail.com>
2021-09-20 16:40:36 +0600

    - Update

--- Runtime/com/rpdev/module/core/view/ModuleView.cs
+++ Runtime/com/rpdev/module/core/view/ModuleView.cs
@@ -1,6 +1,4 @@
 using System;
-using com.rpdev.foundation.module.core.controller;
-using com.rpdev.foundation.module.core.model;
 using com.rpdev.module.common;
 using UnityEngine;
 using Zenject;
@@ -8,32 +6,12 @@ using Zenject;
 namespace com.rpdev.foundation.module.core.view {
 
 	public interface IModuleView : IActiveObject, IDisposable {
-		
-		T           ModuleFacade<T>() where T : IModuleFacade;
 		public void SetPosition(Vector3    position);
 		public void SetRotation(Quaternion rotation);
 	}
 	
 	public class ModuleView : MonoBehaviour, IModuleView, IInitializable {
 
-		private IModuleModel  _model;
-		private bool          _has_model;
-		private IModuleFacade _facade;
-
-		protected T    Model<T>() => (T)_model;
-		protected bool HasModel   => _has_model;
-
-		public T ModuleFacade<T>() where T : IModuleFacade {
-			return (T)_facade;
-		}
-
-		[Inject]
-		public void InjectDependencies(IModuleFacade module_facade, [InjectOptional] IModuleModel module_model) {
-			_model     = module_model;
-			_facade    = module_facade;
-			_has_model = _model != null;
-		}
-		
 		[Inject]
 		public virtual void Initialize() {
 			
@@ -57,7 +35,5 @@ namespace com.rpdev.foundation.module.core.view {
 		
 		
 		public virtual void Dispose() { }
-
-		public class Factory : PlaceholderFactory<DiContainer, ModuleView, InitialModuleViewData, IModuleView> {}
 	}
 }
